e0ffa01c03030e4379b0a809cace7f32b8b846a0
blluv <biluv@protonmail.com>
2024-07-16 00:28:59 +0900

    feature: chat page apply correct text

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,10 +2,14 @@ import SettingIcon from "@assets/icons/settingIcon.svg?react";
 import TranslateIcon from "@assets/icons/translate.svg?react";
 import HomeIcon from "@assets/icons/homeIcon.svg?react";
 import ReportIcon from "@assets/icons/reportIcon.svg?react"
+import VoiceBtnIcon from "@assets/icons/voiceBtn.svg?react";
+import VoiceRecBtnIcon from "@assets/icons/voiceRecBtn.svg?react";
 
 export {
   SettingIcon,
   TranslateIcon,
   HomeIcon,
   ReportIcon,
+  VoiceBtnIcon,
+  VoiceRecBtnIcon
 }
