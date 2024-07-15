9fdd4941e678d5e2a232a72797fc3a0f9f873148
zegiha <leeseoyul070814@gmail.com>
2024-07-15 10:19:18 +0900

    design: Col components  ||  proceeding: adding design system

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -4,6 +4,22 @@ import "@styles/sidebar.css";
 import {Outlet} from "react-router-dom";
 import tmpImage from "@assets/profileTMPImage.jpg";
 import {Row8} from "@components/atomic/rowAndColumns/Row.tsx";
+import {Col4} from "../components/atomic/rowAndColumns/Col.tsx";
+
+function Header() {
+  return (
+    <></>
+  );
+}
+
+function LayoutWithHeader({children}: {children: ReactNode}) {
+  return (
+    <Fragment>
+      <Header />
+      {children}
+    </Fragment>
+  );
+}
 
 function Sidebar() {
   return (
@@ -12,15 +28,11 @@ function Sidebar() {
         <div className="profileImageContainer">
           <img src={tmpImage} alt="profileImage" className="profileImage"/>
         </div>
-
+        <Col4>
+          <span className="text-m-20">이서율</span>
+        </Col4>
       </Row8>
     </div>
-  );
-}
-
-function LayoutWithHeader({children}: {children: ReactNode}) {
-  return (
-    <div>{children}</div>
   );
 }
 
