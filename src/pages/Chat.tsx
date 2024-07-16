bca063d93b1c47739300cf38e4500c5df39675e6
blluv <biluv@protonmail.com>
2024-07-17 04:06:46 +0900

    fix: chat layout

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -124,8 +124,8 @@ function Chat() {
     })
   }
   return (
-    <Col0 padding="46px 0px 0px 0px" maxWidth="900px" margin="0 auto" width="100%" height="calc(100vh - 57px)">
-      <Col20 width="100%" flex="1">
+    <Col0 padding="46px 0px 0px 0px" maxWidth="900px" margin="0 auto" width="100%">
+      <Col20 width="100%" flex="1" padding="0 0 32px 0">
         {chatList.map(e =>
           <ChatBox key={useId()} profileImgUrl={e.profileImgUrl} isMine={e.isMine} messages={e.messages} />
         )}
