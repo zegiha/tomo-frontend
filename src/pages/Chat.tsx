ff06a1969c5bb4c71b18e633030dc40443553277
blluv <biluv@protonmail.com>
2024-07-16 13:54:18 +0900

    refactor: split chatbox

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,85 +1,13 @@
-import { Row8 } from "@/components/atomic/rowAndColumns/Row";
 import styles from "@styles/chat.module.css"
 import classNames from "classnames/bind";
 
-import { TranslateIcon, VoiceBtnIcon, VoiceRecBtnIcon } from "@assets/icons"
+import { VoiceBtnIcon, VoiceRecBtnIcon } from "@assets/icons"
 
-import { Col0, Col20, Col8 } from "@/components/atomic/rowAndColumns/Col";
-import { useEffect, useId, useRef, useState } from "react";
+import { Col0, Col20 } from "@/components/atomic/rowAndColumns/Col";
+import { useId, useRef, useState } from "react";
+import ChatBox from "@/components/chat/ChatBox";
 
 const cn = classNames.bind(styles)
-
-interface IMessage {
-  id: string
-  formattedText: string
-  formattedCorrectText?: string
-  formattedCorrectPronounceText?: string
-}
-
-function BotChatBubble(props: { message: IMessage }) {
-  return <div className={cn("botChatBubble", "text-m-20")} dangerouslySetInnerHTML={{ __html: props.message.formattedText }} />
-}
-
-function MyChatBubble(props: { message: IMessage }) {
-  const needSep = props.message.formattedCorrectText || props.message.formattedCorrectPronounceText
-  return <div className={cn("myChatBubble", "text-m-20")}>
-    <div dangerouslySetInnerHTML={{ __html: props.message.formattedText }} />
-    {
-      needSep && <div className={cn("sep")} />
-    }
-    {
-      props.message.formattedCorrectText && <div dangerouslySetInnerHTML={{ __html: props.message.formattedCorrectText }} />
-    }
-    {
-      props.message.formattedCorrectPronounceText && <div dangerouslySetInnerHTML={{ __html: props.message.formattedCorrectPronounceText }} />
-    }
-  </div>
-}
-
-
-interface TranslateButtonProps {
-  onClick?: () => void
-}
-
-function TranslateButton(props: TranslateButtonProps) {
-  return <div className={cn("translateBtn")}>
-    <TranslateIcon className={cn("translateBtn__icon")} onClick={props.onClick} />
-  </div>
-}
-
-interface ChatBoxProps {
-  profileImgUrl?: string,
-  isMine: boolean
-  messages: IMessage[]
-}
-
-function ChatBox({ isMine, profileImgUrl, messages }: ChatBoxProps) {
-  const [translated, setTranslated] = useState(false)
-
-  const handleTranslate = () => {
-    setTranslated((prev) => !prev)
-  }
-
-  return (
-    <Col8 alignItems={isMine ? 'flex-end' : 'flex-start'}>
-      {profileImgUrl && (
-        <img className={styles.chatProfileImg} src={profileImgUrl} alt="profile-img" />
-      )}
-      {messages.map((msg, i) => {
-        const isLastMessage = i + 1 === messages.length;
-        const ChatBubble = isMine ? MyChatBubble : BotChatBubble;
-
-        return (
-          <Row8 key={msg.id} alignItems="center">
-            {isLastMessage && isMine && <TranslateButton onClick={handleTranslate} />}
-            <ChatBubble message={msg} />
-            {isLastMessage && !isMine && <TranslateButton onClick={handleTranslate} />}
-          </Row8>
-        );
-      })}
-    </Col8>
-  );
-}
 
 
 class Voice {
@@ -143,7 +71,29 @@ function Chat() {
         id: "hnybiyu",
         formattedText: "abcd",
       }]
-    }
+    },
+    {
+      isMine: false,
+      profileImgUrl: "https://r2.etty.dev/bb.jpg",
+      messages: [{
+        id: "aaaaaa",
+        formattedText: "abcd",
+      }, {
+        id: "bbbbbb",
+        formattedText: "abcd",
+      }]
+    },
+    {
+      isMine: false,
+      profileImgUrl: "https://r2.etty.dev/bb.jpg",
+      messages: [{
+        id: "aaaaaa",
+        formattedText: "abcd",
+      }, {
+        id: "bbbbbb",
+        formattedText: "abcd",
+      }]
+    },
   ]
 
   const [isRecording, setIsRecording] = useState(false)
@@ -174,7 +124,7 @@ function Chat() {
     })
   }
   return (
-    <Col0 padding="46px 20px 0px 20px" maxWidth="900px" margin="0 auto" width="100%">
+    <Col0 padding="46px 0px 0px 0px" maxWidth="900px" margin="0 auto" width="100%" height="calc(100vh - 57px)">
       <Col20 width="100%" flex="1">
         {chatList.map(e =>
           <ChatBox key={useId()} profileImgUrl={e.profileImgUrl} isMine={e.isMine} messages={e.messages} />
